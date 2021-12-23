c17601371942bed6c87f8f021d21330286a1b23c
v4rsh1th <varshithjay6@gmail.com>
2021-12-23 11:47:31 +0530

    modifs

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def home():
 def about():
   return render_template('about.html')
 
+@app.route("/gallery")
+def gallery():
+  return render_template('gallery.html')
+
 @app.route("/contact")
 def contact():
   return render_template('contact.html')
