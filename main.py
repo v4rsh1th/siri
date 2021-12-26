9900940feb280d42dc5fd45d1e7758deb6f57380
v4rsh1th <varshithjay6@gmail.com>
2021-12-26 15:24:01 +0530

    modifs

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
 
+@app.route("/services")
+def services():
+  return render_template('services.html')
+
 @app.route("/about")
 def about():
   return render_template('about.html')
